e94beb5d7008291a2a63d57198aa5a9dfc18c9e9
lwq945 <lwq945@icloud.com>
2018-03-28 15:40:49 +0800

    给店铺和商品添加一个是否选中属性（响应式）

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -20,8 +20,19 @@ new Vue({
     methods: {
         getCartLists() {
             axios.post(url.cartList).then(res => {
-                this.cartLists = res.data.cartLists
+                let tempLists = res.data.cartLists   //接收获取到的原始数据
+                
+                tempLists.forEach((shop) => {
+                    shop.checked = true
+                    shop.goodsLists.forEach((good) => {   //遍历原始数据，给每个商品添加一个是否选中属性
+                        good.checked = true
+                    })
+                });
+                this.cartLists = tempLists  //再赋值
             })
+        },
+        selectGood(good) {
+            good.checked = !good.checked
         }
     },
     computed: {
