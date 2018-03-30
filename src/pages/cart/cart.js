0d249ce39b830d85de7fb2d707007cf79959dffe
lwq945 <lwq945@icloud.com>
2018-03-30 17:28:13 +0800

    添加购物车页面编辑状态下的选择功能

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -14,8 +14,8 @@ new Vue({
     data: {
         cartLists: null,
         total: 0,
-        editShop: null,
-        editShopIndex: -1
+        editingShop: null,
+        editingShopIndex: -1
     },
     created() {
         this.getCartLists()
@@ -39,7 +39,7 @@ new Vue({
                 })
             }
         },
-        selectLists() { //选中的商品列表
+        selectLists() { //正常状态下选中的商品列表
             if(this.cartLists && this.cartLists.length) {
                 let selectArr = []
                 let total = 0
@@ -55,7 +55,36 @@ new Vue({
                 return selectArr
             }
             return []
+        },
+        allRemoveSelected: {
+            get() {
+                if(this.editingShop){
+                    return  this.editingShop.removeChecked     //返回处于编辑状态店铺的removeChecked
+                }
+                return false
+            },
+            set(newVal) {
+                if(this.editingShop){
+                    this.editingShop.removeChecked = newVal
+                    this.editingShop.goodsLists.forEach( good => {
+                        good.removeChecked = newVal
+                    })
+                }
+            }
+        },
+        removeLists() {  //编辑状态下选中的删除商品列表
+            if(this.editingShop) {
+                let removeSelectArr = []
+                this.editingShop.goodsLists.forEach(good => {
+                    if(good.removeChecked){
+                        removeSelectArr.push(good)
+                    }
+                })
+                return removeSelectArr
+            }
+            return []
         }
+
     },
     methods: {
         getCartLists() {
@@ -63,30 +92,35 @@ new Vue({
                 let tempLists = res.data.cartLists   //接收获取到的原始数据
                 
                 tempLists.forEach((shop) => {
-                    shop.checked = true
+                    shop.checked = true   //正常状态的选择
+                    shop.removeChecked = false  //编辑状态的选择
                     shop.editing = false     //是否处于编辑状态
-                    shop.editMsg = "编辑"  //正常状态信息是‘编辑’，编辑状态是‘完成’
+                    shop.editMsg = "编辑"  //正常状态信息是‘编辑’，编辑状态信息是‘完成’
                     shop.goodsLists.forEach((good) => {   //遍历原始数据，给每个商品添加一个是否选中属性
                         good.checked = true
+                        good.removeChecked = false
                     })
                 });
                 this.cartLists = tempLists  //再赋值
             })
         },
         selectGood(shop,good) {
-            good.checked = !good.checked
-            shop.checked = shop.goodsLists.every( good => {   //店铺的选中由它底下的所有商品的选中状态决定，遍历该店铺的所有商品，是否都是选中，若都选中，则店铺也处在选中状态
-                return good.checked
+           let attr = this.editingShop? 'removeChecked' : 'checked'  //判断是否处于编辑状态
+            good[attr] = !good[attr]
+            shop[attr] = shop.goodsLists.every( good => {   //店铺的选中由它底下的所有商品的选中状态决定，遍历该店铺的所有商品，是否都是选中，若都选中，则店铺也处在选中状态
+                return good[attr]
             })
         },
         selectShop(shop) {
-            shop.checked = !shop.checked  //取反
+            let attr = this.editingShop? 'removeChecked' : 'checked'  //判断是否处于编辑状态
+            shop[attr] = !shop[attr]  //取反
             shop.goodsLists.forEach(good => {  //遍历所有商品，商品的选中状态由店铺的选中决定，当店铺被选中，它底下的所有商品都被选中
-                good.checked = shop.checked
+                good[attr] = shop[attr]
             })
         },
         selectAll() {
-            this.allSelected = !this.allSelected
+            let attr = this.editingShop? 'allRemoveSelected' : 'allSelected'  //判断是否处于编辑状态
+            this[attr] = !this[attr]
         },
         edit(shop,shopIndex) {
             shop.editing = !shop.editing
@@ -97,8 +131,8 @@ new Vue({
                     item.editMsg = shop.editing ? '' : '编辑'
                 }
                 
-                this.editShop = shop.editing ? shop : null    //全局变量，用来处理不是数据列表中的数据渲染(获取处于编辑状态的店铺)
-                this.editShopIndex = shop.editing ? shopIndex : -1  //获取处于编辑状态的店铺的下标
+                this.editingShop = shop.editing ? shop : null    //全局变量，用来处理不是数据列表中的数据渲染(获取处于编辑状态的店铺)
+                this.editingShopIndex = shop.editing ? shopIndex : -1  //获取处于编辑状态的店铺的下标
             })
 
         }
