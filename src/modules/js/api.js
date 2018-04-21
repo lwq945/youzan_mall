5f66dca38ea5d51700b2eb1aa154901773347993
lwq945 <lwq945@icloud.com>
2018-04-21 22:59:12 +0800

    更换mock数据接口为rap

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -1,36 +1,79 @@
-let url =  {
-    hostLists: ['get','index/hotLists'],
-    banner: ['get','index/banner'],
-    topLists: ['get','category/topLists'],
-    rank: ['get','category/rank'],
-    subLists: ['post','category/subLists'],
-    searchList: ['post','search/list'],
-    detailLists: ['get','goods/details'],
-    dealLists: ['get','goods/deal'],
-    addCart: ['post','cart/add'],
-    cartList: ['get','cart/list'],
-    cartReduce: ['post','cart/reduce'],
-    cartAdd: ['post','cart/Add'],
-    cartRemove: ['post','cart/remove'],
-    cartMRemove: ['post','cart/mremove'],
-    cartUpdate: ['post','cart/update'],
-    addressList: ['post','address/list'],
-    addressAdd: ['post','address/add'],
-    addressRemove: ['post','address/remove'],
-    addressUptate: ['post','address/update'],
-    addressDefault: ['post','address/setDefault']
+//rap2
+// let url =  {
+//     hostLists: ['get','index/hotLists'],
+//     banner: ['get','index/banner'],
 
+//     topLists: ['get','category/topLists'],
+//     rank: ['get','category/rank'],
+//     subLists: ['post','category/subLists'],
 
-}
+//     searchList: ['post','search/list'],
+
+//     detailLists: ['get','goods/details'],
+//     dealLists: ['get','goods/deal'],
+
+//     addCart: ['post','cart/add'],
+//     cartList: ['get','cart/list'],
+//     cartReduce: ['post','cart/reduce'],
+//     cartAdd: ['post','cart/Add'],
+//     cartRemove: ['post','cart/remove'],
+//     cartMRemove: ['post','cart/mremove'],
+//     cartUpdate: ['post','cart/update'],
+
+//     addressList: ['post','address/list'],
+//     addressAdd: ['post','address/add'],
+//     addressRemove: ['post','address/remove'],
+//     addressUptate: ['post','address/update'],
+//     addressDefault: ['post','address/setDefault']
+// }
+
 
 // http://rap2api.taobao.org/app/mock/8405/get/index/hotLists
-let host = 'http://rap2api.taobao.org/app/mock/8405/'
+// let host = 'http://rap2api.taobao.org/app/mock/8405/'
+
+// for (let key in url) {
+//     if (url.hasOwnProperty(key)) {
+//          url[key] = host + url[key][0] + '/' + url[key][1];
+//     }
+// }
+
+
+//rap
+let url = {
+    hostLists: '/index/hostLists',
+    banner: '/index/banner',
+    topLists: '/category/topList',
+    rank: '/category/rank',
+    subLists: '/category/subList',
+    searchList: '/search/list',
+    dealLists: '/goods/deal',
+    detailLists: '/goods/details',
+
+    addCart: '/cart/add',
+    cartList: '/cart/list',
+    cartReduce: '/cart/reduce',
+    cartAdd: '/cart/add',
+    cartRemove: '/cart/remove',
+    cartMRemove: '/cart/mremove',
+    cartUpdate: '/cart/update',
+
+    addressList: '/address/list',
+    addressAdd: '/address/add',
+    addressRemove: '/address/remove',
+    addressUptate: '/address/update',
+    addressDefault: '/address/setDefault'
+
+
+}
+//http://rapapi.org/mockjsdata/31314/index/hostLists
+let host = 'http://rapapi.org/mockjsdata/31314'
 
 for (let key in url) {
-    if (url.hasOwnProperty(key)) {
-         url[key] = host + url[key][0] + '/' + url[key][1];
+    if(url.hasOwnProperty(key)) {
+        url[key] = host + url[key]
     }
 }
+
 
 
 export default url
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -36,24 +36,24 @@ let app = new Vue({
                 pageNum: this.pageNum,
                 pageSize: this.pageSize
             }).then(res => {
-                this.lists = res.data.lists
+                
                 //当前获取到的数据
-                // let curLists = res.data.lists
-                // // console.log(curLists.length)
-                // // //判断所有数据是否加载完成
-                // if(curLists.length < this.pageSize) {
-                //     this.allLoaded = true
-                // }
-                // if(this.lists){
-                //     //已获取的数据和当前获取的数据合并
-                //     this.lists = this.lists.concat(curLists)
-                // }else{
-                //     //第一次获取的数据
-                //     this.lists = curLists
-                // }
-                // //下次可以请求数据
-                // this.loading = false
-                // this.pageNum++
+                let curLists = res.data.lists
+                // console.log(curLists.length)
+                // //判断所有数据是否加载完成
+                if(curLists.length < this.pageSize) {
+                    this.allLoaded = true
+                }
+                if(this.lists){
+                    //已获取的数据和当前获取的数据合并
+                    this.lists = this.lists.concat(curLists)
+                }else{
+                    //第一次获取的数据
+                    this.lists = curLists
+                }
+                //下次可以请求数据
+                this.loading = false
+                this.pageNum++
             }) 
         },
         getBanner() {
